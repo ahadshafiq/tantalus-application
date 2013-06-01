9cd85107ea6bd60499d27d7e7bc2d6ddf2417ffc
Ahad Shafiq <ahad.shafiq@gmail.com>
2013-06-01 17:57:01 +0300

    initial ommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twt::Application.config.secret_token = 'c56d9b5a378df6ac944d9df63c63fcbbbf261d29fcfd0da71bd997e15a18ae4d2df1fd8e2b5ddc48ccf6fe42a7489985404e41d26e8ef4f1d922be78f70873f8'
